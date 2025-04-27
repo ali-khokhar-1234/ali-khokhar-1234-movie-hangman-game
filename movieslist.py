5a2b684a71b1e6ec1482cba5a1a4ee31c9ef0d7c
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-04-27 10:41:31 -0400

    movieslist.py

--- movieslist.py
+++ movieslist.py
@@ -1,6 +1,9 @@
-movies = ("teefa in trouble", "aliens", "the avengers", "barbie", "casino", "cinderella",
-          "dune", "deadpool", "gladiator", "goodfellas", "inception",
-          "interstellar", "jaws", "kal ho naa ho", "logan", "oppenheimer",
-          "parasite", "ratatouille", "scarface", "serendipity", "seven", "shrek",
-          "sinners",  "superman", "titanic", "twilight", "whiplash",
-          "wicked")
+movies = ("aliens", "the avengers", "back to the future", "barbie", "casino", "cinderella", "the dark knight",
+          "dune", "deadpool", "fight club", "gladiator", "goodfellas", "good will hunting", "the grand budapest hotel", 
+          "the green mile", "heat", "harry potter", "how to train your dragon", "inglourious basterds", "inception", 
+          "the incredibles", "interstellar", "jaws", "kal ho naa ho", "khabie khushi khabi gham", "kuch kuch hota hai", 
+          "the legend of maula jatt", "the lion king", "the lion the witch and the wardrobe", "logan",  "lord of the rings", 
+          "the matrix", "mulan", "om shanti om", "oppenheimer", "parasite", "pirates of the caribbean", "raiders of the lost ark",
+          "ratatouille", "scarface", "serendipity", "seven",  "the shawshank redemption", "shrek", "sinners", "star wars", 
+          "superman", "taare zameen par", "teefa in trouble", "the terminator",  "top gun", "thunderbolts", "titanic", 
+          "toy story", "twilight", "whiplash", "whisper of the heart", "wicked", "the wild robot")
