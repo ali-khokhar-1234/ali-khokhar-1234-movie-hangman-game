ed8cca302463b61a626578b28554855f18062751
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-04-27 10:17:17 -0400

    Add files via upload

---
+++ movieslist.py
@@ -0,0 +1,6 @@
+movies = ("teefa in trouble", "aliens", "the avengers", "barbie", "casino", "cinderella",
+          "dune", "deadpool", "gladiator", "goodfellas", "inception",
+          "interstellar", "jaws", "kal ho naa ho", "logan", "oppenheimer",
+          "parasite", "ratatouille", "scarface", "serendipity", "seven", "shrek",
+          "sinners",  "superman", "titanic", "twilight", "whiplash",
+          "wicked")
