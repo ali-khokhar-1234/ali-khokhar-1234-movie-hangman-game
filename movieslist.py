cca14a00e74b2d100818b91059cd663f9ea42564
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-05-09 13:35:38 -0400

    Update movieslist.py

--- movieslist.py
+++ movieslist.py
@@ -1,8 +1,8 @@
-movies = ("aliens", "amelie", "anora", "avengers", "barbie", "batman", "cars", "casino",
-          "cinderella", "coco", "conclave", "dangal", "dune", "deadpool", "flow",
-          "gladiator", "goodfellas", "heat", "incendies", "inception", "interstellar",
-          "jaws", "laagan", "logan", "matrix", "midsommar", "mulan", "network", "nosferatu",
-          "oldboy", "oppenheimer", "parasite", "prisoners", "ratatouille", "scarface",
-          "serendipity", "seven", "sholay", "shrek", "sinners", "superman", "swades",
-          "terminator", "thunderbolts", "titanic", "twilight",
-          "whiplash", "wicked",)
+movies = ("aliens", "amelie", "anora", "avengers", "barbie", "batman", "cars",
+          "casino", "cinderella", "coco", "conclave", "dangal", "dune", "deadpool", 
+          "flow", "gladiator", "goodfellas", "heat", "incendies", "inception", 
+          "interstellar", "ironman", "jaws", "laagan", "logan", "matrix", "midsommar", 
+          "mulan", "network", "nosferatu", "oldboy", "oppenheimer", "parasite", 
+          "prisoners", "ratatouille", "scarface","serendipity", "seven", "sholay", 
+          "shrek", "sinners", "superman", "swades", "terminator", "thunderbolts",
+          "titanic", "twilight", "whiplash", "wicked",)
